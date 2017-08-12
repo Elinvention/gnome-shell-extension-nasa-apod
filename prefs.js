635752e643d9265ebfcb087bfc5e5d4d39528b4a
Elia Argentieri <elia.argentieri@openmailbox.org>
2017-08-12 02:56:55 +0200

    Add a new page in settings to list downloaded images in the cache and to set them as background.
    If you don't like the picture of the day, now you can set an older one easily!

--- prefs.js
+++ prefs.js
@@ -4,6 +4,8 @@ const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const Utils = Me.imports.utils;
+const GdkPixbuf = imports.gi.GdkPixbuf;
+
 
 let settings;
 
@@ -11,11 +13,31 @@ function init() {
     settings = Utils.getSettings(Me);
 }
 
+function buildCacheListBoxRow(title, path) {
+    let buildable = new Gtk.Builder();
+    buildable.add_objects_from_file(Me.dir.get_path() + '/Settings.ui', ["cache_listboxrow"]);
+    let row = buildable.get_object("cache_listboxrow");
+    let event = row.get_child();
+    let box = event.get_child();
+    let label = box.get_children()[0];
+    let image = box.get_children()[1];
+    event.connect('button-press-event', function(widget, event) {
+        Utils.setBackgroundBasedOnSettings(path, settings);
+    });
+    let stream = Gio.file_new_for_path(path).read(null);
+    GdkPixbuf.Pixbuf.new_from_stream_at_scale_async(stream, 200, 200, true, null, function(source, res) {
+        let pix = GdkPixbuf.Pixbuf.new_from_stream_finish(res);
+        image.set_from_pixbuf(pix);
+    });
+    label.set_text(title);
+    return row;
+}
+
 function buildPrefsWidget(){
 
     // Prepare labels and controls
     let buildable = new Gtk.Builder();
-    buildable.add_from_file( Me.dir.get_path() + '/Settings.ui' );
+    buildable.add_objects_from_file(Me.dir.get_path() + '/Settings.ui', ['prefs_widget']);
     let box = buildable.get_object('prefs_widget');
 
     buildable.get_object('extension_version').set_text(Me.metadata.version.toString());
@@ -27,6 +49,8 @@ function buildPrefsWidget(){
     let lsSwitch = buildable.get_object('lock_screen');
     let fileChooser = buildable.get_object('download_folder');
     let apiEntry = buildable.get_object('api_key');
+    let cacheListBox = buildable.get_object('cache_listbox');
+    let cacheScroll = buildable.get_object('cache_scroll');
 
     // Indicator
     settings.bind('hide', hideSwitch, 'active', Gio.SettingsBindFlags.DEFAULT);
@@ -55,6 +79,35 @@ function buildPrefsWidget(){
     settings.connect('changed::api-key', function() {
         if (settings.get_string('api-key') == "")
             settings.reset('api-key');
+    });
+
+    let downloadFolder = settings.get_string('download-folder');
+    let dir = Gio.file_new_for_path(downloadFolder);
+    let files_iter = dir.enumerate_children('standard::name', Gio.FileQueryInfoFlags.NONE, null);
+
+    function load_files_thumbnails() {
+        files_iter.next_files_async(10, 0, null, function(source, res) {
+            let file_infos = source.next_files_finish(res);
+            file_infos.forEach(function(file, index) {
+                try {
+                    let path = downloadFolder + "/" + file.get_name();
+                    Utils.log("Loading: " + path);
+                    let row = buildCacheListBoxRow(file.get_name(), path);
+                    cacheListBox.add(row);
+                } catch (err) {
+                    Utils.log(err.message);
+                    return;
+                }
+            });
+        });
+    };
+
+    load_files_thumbnails();
+
+    cacheScroll.connect('edge-reached', function(window, pos) {
+        if (pos == 3) {  // if user reached the bottom of the SrolledWindow
+            load_files_thumbnails();
+        }
     });
 
     box.show_all();
