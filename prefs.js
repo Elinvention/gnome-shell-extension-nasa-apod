0cb92a975034bfe715ee238ebee8c45a27ede240
Elia Argentieri <elia.argentieri@openmailbox.org>
2017-09-02 01:36:54 +0200

    Load cache page only if user opens it

--- prefs.js
+++ prefs.js
@@ -16,9 +16,22 @@ function init() {
     css.load_from_path(Me.dir.get_path() + "/theme.css");
 }
 
-function buildCacheFlowBoxChild(title, date, path) {
+function buildCacheFlowBoxChild(folder, file) {
+    let path = folder + file;
+    let split = file.split('-');
+    let date = split.splice(0, 3).join('-');
+    split = split.join('-').split('.');
+    let extension = split.pop();
+    let title = split.join('-');
+
+    if (['jpg', 'png', 'gif'].indexOf(extension) < 0)
+        throw path + " is not an image";
+
+    Utils.log("Loading: " + path);
+
     let buildable = new Gtk.Builder();
     buildable.add_objects_from_file(Me.dir.get_path() + '/Settings.ui', ["cache_flowchild"]);
+
     let row = buildable.get_object("cache_flowchild");
     let event = buildable.get_object("event");
     let title_label = buildable.get_object("title");
@@ -27,11 +40,13 @@ function buildCacheFlowBoxChild(title, date, path) {
     event.connect('button-press-event', function(widget, event) {
         Utils.setBackgroundBasedOnSettings(path, settings);
     });
+
     let stream = Gio.file_new_for_path(path).read(null);
     GdkPixbuf.Pixbuf.new_from_stream_at_scale_async(stream, 200, 200, true, null, function(source, res) {
         let pix = GdkPixbuf.Pixbuf.new_from_stream_finish(res);
         image.set_from_pixbuf(pix);
     });
+
     title_label.set_text(title);
     date_label.set_text(date);
     row.get_style_context().add_provider(css, 0);
@@ -43,7 +58,7 @@ function buildPrefsWidget(){
     // Prepare labels and controls
     let buildable = new Gtk.Builder();
     buildable.add_objects_from_file(Me.dir.get_path() + '/Settings.ui', ['prefs_widget']);
-    let box = buildable.get_object('prefs_widget');
+    let notebook = buildable.get_object('prefs_widget');
 
     buildable.get_object('extension_version').set_text(Me.metadata.version.toString());
 
@@ -71,10 +86,11 @@ function buildPrefsWidget(){
         transientSwitch.set_sensitive(settings.get_boolean('notify'));
     });
 
+    // Wallpaper and lock screen
     settings.bind('set-background', bgSwitch, 'active', Gio.SettingsBindFlags.DEFAULT);
     settings.bind('set-lock-screen', lsSwitch, 'active', Gio.SettingsBindFlags.DEFAULT);
 
-    //download folder
+    // Download folder
     fileChooser.set_filename(downloadFolder);
     fileChooser.add_shortcut_folder_uri("file://" + GLib.get_user_cache_dir() + "/apod");
     fileChooser.connect('file-set', function(widget) {
@@ -88,45 +104,46 @@ function buildPrefsWidget(){
             settings.reset('api-key');
     });
 
-    let dir = Gio.file_new_for_path(downloadFolder);
-    let files_iter = dir.enumerate_children('standard::name', Gio.FileQueryInfoFlags.NONE, null);
-    let file_names = [], file;
-    while ((file = files_iter.next_file(null)) != null) {
-        file_names.push(file.get_name());
-    }
-    file_names.sort();
+    // Cache page
+    function loadCachePage() {
+        let dir = Gio.file_new_for_path(downloadFolder);
+        let files_iter = dir.enumerate_children('standard::name', Gio.FileQueryInfoFlags.NONE, null);
+        let file_names = [], file;
+        while ((file = files_iter.next_file(null)) != null) {
+            file_names.push(file.get_name());
+        }
+        file_names.sort();
 
-    function load_files_thumbnails() {
-        let file, i = 0;
-        while ((file = file_names.pop()) != null && i < 10) {
-            try {
-                let path = downloadFolder + file;
-                let split = file.split('-');
-                let date = split.splice(0, 3).join('-');
-                split = split.join('-').split('.');
-                split.pop();  // drop the extension
-                let title = split.join('-');
-                Utils.log("Loading: " + path);
-                let child = buildCacheFlowBoxChild(title, date, path);
-                cacheFlowBox.add(child);
-            } catch (err) {
-                Utils.log(err.message);
-            } finally {
-                i++;
-            }
+        function load_files_thumbnails() {
+            let file, i = 0;
+            while ((file = file_names.pop()) != null && i < 10) {
+                try {
+                    let child = buildCacheFlowBoxChild(downloadFolder, file);
+                    cacheFlowBox.add(child);
+                } catch (err) {
+                    Utils.log(err);
+                } finally {
+                    i++;
+                }
+            };
         };
+
+        load_files_thumbnails();
+
+        cacheScroll.connect('edge-reached', function(window, pos) {
+            if (pos == 3) {  // if user reached the bottom of the SrolledWindow
+                load_files_thumbnails();
+            }
+        });
     };
 
-    load_files_thumbnails();
-
-    cacheScroll.connect('edge-reached', function(window, pos) {
-        if (pos == 3) {  // if user reached the bottom of the SrolledWindow
-            load_files_thumbnails();
-        }
+    notebook.connect('switch-page', function(widget, page, page_index) {
+        if (page_index == 1)
+            loadCachePage();
     });
 
-    box.show_all();
+    notebook.show_all();
 
-    return box;
+    return notebook;
 };
 
