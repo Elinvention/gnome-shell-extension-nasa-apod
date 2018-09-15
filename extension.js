fa253423fd0db277c8d077d010bc3c73daf0c5dd
Thibault Lemaire <thibault.lemaire@zoho.eu>
2018-09-15 23:32:10 +0200

    Add my key to default keys

--- extension.js
+++ extension.js
@@ -28,6 +28,11 @@ const RETRY_RATE_LIMIT_SECONDS = 60 * 30;
 const RETRY_NETWORK_UNAVAILABLE = 60;
 const ICON = "saturn";
 
+const DefaultApiKeys = [
+    "XKSoS8Bv05ij8JH8UWa7eqMavXgGfFStcc6Pu3KH",
+    "jCUjMOBpL523SxLoi4PogFZ3YsvvFtVNyEvRd0IB"
+]
+
 
 let nasaApodIndicator;
 let httpSession = new Soup.SessionAsync();
